f7910dd536082c2b80d2969324f20bb794884ffc
Adrian Lohr <adrian.lohr@online.de>
2022-05-22 02:01:16 +0200

    added custom build script

---
+++ build_rom.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+arm-none-eabi-gcc -c main.c -fno-strict-aliasing -mthumb-interwork -mthumb -O2 -o main.o
+arm-none-eabi-gcc -c main.c -mthumb-interwork -mthumb -O2 -o main.o
+arm-none-eabi-gcc -c main.c -fno-strict-aliasing -mthumb-interwork -mthumb -O2 -o main.o
+arm-none-eabi-gcc main.o -mthumb-interwork -mthumb -specs=gba.specs -o main.elf
+arm-none-eabi-objcopy -v -O binary main.elf main.gba
+gbafix main.gba
+mgba-qt main.gba
